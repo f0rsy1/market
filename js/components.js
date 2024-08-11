131fa46256120c588509a162e9c2d5238b890e99
f0rsy1 <smalinin716@gmail.com>
2024-08-11 11:31:25 +0300

    init repository

---
+++ js/components.js
@@ -0,0 +1,20 @@
+class ProductCard extends HTMLElement {
+constructor(name,img,price) {
+super();
+this.name = name;
+this.img = img;
+this.price = price;
+}
+
+connectedCallback() {
+this.innerHTML = `<div id="name">
+${this.name}
+</div>
+<img src ="${this.img}">
+<div id="price-holder">
+Цена: <span id="price">${this.price}</span> рублей
+</div>`;
+}
+}
+
+customElements.define("product-card",ProductCard);
